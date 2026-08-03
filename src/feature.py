a109d401a5e4b301f83fe63be96d2e2ab73d5732
chc216 <mine3386@naver.com>
2026-08-03 16:54:30 +0900

    exp: Parch 이진화 적용, 효과 없음

--- src/feature.py
+++ src/feature.py
@@ -26,8 +26,10 @@ def prep_baseline():
     )
 
 
+# Age -> 그룹 범주형으로 나눠서 파이프라인 적용
+
 def bin_age(X):
-    return pd.DataFrame(pd.cut( X.squeeze(), bins=[-1,20,40,100], labels=[0,1,2]))
+    return pd.DataFrame(pd.cut( X.squeeze(), bins=[-1,20,40,100], labels=False))
 
 def prep_v2():
     age = ["Age"]
@@ -46,6 +48,40 @@ def prep_v2():
     ])
     return ColumnTransformer([
         ("num", num_pipeline, number),
+        ("cat", cat_pipeline, cat),
+        ("age", age_pipeline, age)
+    ])
+
+
+
+
+def to_binary_cat(X):
+    return pd.DataFrame(pd.cut(X.squeeze(), bins=[-1,0,14], labels=[0,1]))
+# 기존 Age 변경에 더하여 Parch특성을 이진 범주로 가공하는 파이프라인
+def prep_v3():
+    age = ["Age"]
+    parch = ["Parch"]
+    number = ["Pclass"]
+    cat = ["Sex"]
+
+    age_transformer = FunctionTransformer(bin_age)
+    age_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median")),
+                             ("binner", age_transformer)])
+
+    parch_transformer = FunctionTransformer(to_binary_cat)
+    parch_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="median")),
+        ("transformer", parch_transformer)
+    ])
+    num_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median"))])
+
+    cat_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder())
+    ])
+    return ColumnTransformer([
+        ("num", num_pipeline, number),
+        ("parch", parch_pipeline, parch),
         ("cat", cat_pipeline, cat),
         ("age", age_pipeline, age)
     ])
