916d90258ae654871e1ab1af7394699e5e390c36
chc216 <mine3386@naver.com>
2026-08-04 11:10:14 +0900

    exp: 실험 추가

--- README.md
+++ README.md
@@ -25,11 +25,19 @@
 * 특이사항
   * 핸즈온 머신러닝 2장 적용
 
-| 버전                       | 사용 피처                  | CV(k-fold) | LB |
-|--------------------------|------------------------|------------|----|
+| 버전                       | 사용 피처                   | CV(k-fold) | LB |
+|--------------------------|-------------------------|------------|----|
 | v1 - logistic regression | Age, Parch, Pclass, Sex | 0.7893     | -  |
-| v1 - random forest       | 위와 동일                  | 0.7893     | -  |
-| v2 - logistic regression | v1에서 Age를 범주형으로 변경     | 0.8019     | -  |
-| v2 - random forest       | 위와 동일                  | 0.8047     | -  |
-|                          |                        |            |    |
+| v1 - random forest       | 위와 동일                   | 0.7893     | -  |
+| v2 - logistic regression | v1에서 Age를 범주형으로 변경      | 0.8019     | -  |
+| v2 - random forest       | 위와 동일                   | 0.8047     | -  |
+| v3 - logistic regression | v2에서 Parch를 범주형으로 변경    | 0.7977     |    |
+| v3 - random forest | 위와 동일                   | 0.8005| |
+| v4 - logistic regression | v3에서 이름 경칭 특성 추가        | 0.8159 ||
+| v4 - random forest | 위와 동일                   | 0.7864 ||
+| v4-2 - randomforest | Age를 숫자형으로 사용           | 0.7879 ||
+| v5 - logistic regression | has_cabin 이진 범주형 추가     | 0.8229 ||
+| v5 - random forest | 위와 동일                   |0.7879 ||
+|                          |                         |            |    |
+
 
--- src/feature.py
+++ src/feature.py
@@ -1,9 +1,11 @@
+from pandas.core.interchange.dataframe_protocol import DataFrame
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 import pandas as pd
+import numpy as np
 
 
 
@@ -84,4 +86,141 @@ def prep_v3():
         ("parch", parch_pipeline, parch),
         ("cat", cat_pipeline, cat),
         ("age", age_pipeline, age)
+    ])
+
+
+
+# 이름의 경칭을 추출하여 특성으로 활용하는 파이프라인
+def extract_title(X):
+    title = X.squeeze().str.extract(r",\s*([^\.]+)\.")
+    common = ["Mr", "Miss", "Mrs", "Master"]
+    return pd.DataFrame(title.where(title.isin(common), "Rare"))
+
+def prep_v4():
+    age = ["Age"]
+    parch = ["Parch"]
+    pclass = ["Pclass"]
+    cat = ["Sex"]
+    cat_name = ["Name"]
+
+    name_transformer = FunctionTransformer(extract_title)
+    name_pipeline = Pipeline([
+        ("transformer", name_transformer),
+        ("onehot", OneHotEncoder())
+    ])
+
+    age_transformer = FunctionTransformer(bin_age)
+    age_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median")),
+                             ("binner", age_transformer)])
+
+    parch_transformer = FunctionTransformer(to_binary_cat)
+    parch_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="median")),
+        ("transformer", parch_transformer)
+    ])
+    num_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median"))])
+
+    cat_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder())
+    ])
+    return ColumnTransformer([
+        ("num", num_pipeline, pclass),
+        ("parch", parch_pipeline, parch),
+        ("cat", cat_pipeline, cat),
+        ("age", age_pipeline, age),
+        ("name", name_pipeline, cat_name)
+    ])
+
+
+def prep_v4_2():
+    age = ["Age"]
+    parch = ["Parch"]
+    pclass = ["Pclass"]
+    cat = ["Sex"]
+    cat_name = ["Name"]
+
+    name_transformer = FunctionTransformer(extract_title)
+    name_pipeline = Pipeline([
+        ("transformer", name_transformer),
+        ("onehot", OneHotEncoder())
+    ])
+
+    age_transformer = FunctionTransformer(bin_age)
+    age_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median")),
+                             ("scaler", StandardScaler()),])
+
+    parch_transformer = FunctionTransformer(to_binary_cat)
+    parch_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="median")),
+        ("transformer", parch_transformer)
+    ])
+    num_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median"))])
+
+    cat_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder())
+    ])
+    return ColumnTransformer([
+        ("num", num_pipeline, pclass),
+        ("parch", parch_pipeline, parch),
+        ("cat", cat_pipeline, cat),
+        ("age", age_pipeline, age),
+        ("name", name_pipeline, cat_name)
+    ])
+
+
+def extract_cabin_to_binary(X):
+    filled = X.fillna('N')
+    df =  pd.DataFrame(
+        np.where(filled == 'N', 'N', 'Y'),
+    )
+    return df
+
+
+def prep_v5():
+    age = ["Age"]
+    parch = ["Parch"]
+    pclass = ["Pclass"]
+    cat = ["Sex"]
+    cat_name = ["Name"]
+    cabin = ["Cabin"]
+
+
+    ## 객실 특성 변환 과정
+    # 1. 결측치는 모두 N으로 처리한다. 2. 나머지는 모두 1로 처리한다. -> 원핫 인코딩을 해본다.
+    cabin_transformer = FunctionTransformer(extract_cabin_to_binary)
+    cabin_pipeline = Pipeline([
+        ("transformer", cabin_transformer),
+        ("onehot", OneHotEncoder())
+    ])
+
+    name_transformer = FunctionTransformer(extract_title)
+    name_pipeline = Pipeline([
+        ("transformer", name_transformer),
+        ("onehot", OneHotEncoder())
+    ])
+
+    age_transformer = FunctionTransformer(bin_age)
+    age_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median")),
+                             ("scaler", StandardScaler()), ])
+
+    parch_transformer = FunctionTransformer(to_binary_cat)
+    parch_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="median")),
+        ("transformer", parch_transformer)
+    ])
+    num_pipeline = Pipeline([("imputer", SimpleImputer(strategy="median"))])
+
+    cat_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("onehot", OneHotEncoder())
+    ])
+    return ColumnTransformer([
+        ("num", num_pipeline, pclass),
+        ("parch", parch_pipeline, parch),
+        ("cat", cat_pipeline, cat),
+        ("age", age_pipeline, age),
+        ("name", name_pipeline, cat_name),
+        ("has_cabin", cabin_pipeline, cabin)
     ])
